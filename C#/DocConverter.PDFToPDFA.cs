b173f5d5c714031635a24c85e54677fe28364d4e
Bruce Tompkinson <bruce.tompkinson@activepdf.com>
2020-09-10 10:51:17 -0700

    Added PDF to PDF/A examples

---
+++ C#/DocConverter.PDFToPDFA.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2019 ActivePDF, Inc.
+// ActivePDF DocConverter
+
+using System;
+
+// Make sure to add the ActivePDF product .NET DLL(s) to your application.
+// .NET DLL(s) are typically found in the products 'bin' folder.
+namespace DocConverterExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            // Instantiate Object
+            APDocConverter.DocConverter docConverter =
+                new APDocConverter.DocConverter();
+
+            // Enable extra logging (logging should only be used while
+            // troubleshooting) C:\ProgramData\activePDF\Logs\
+            docConverter.Debug = true;
+
+            // Convert the Word document from PDF to another format using Solid
+            // Documents SDK. The second parameter determines the output file
+            // format
+            DCDK.Results.DocConverterResult result =
+                docConverter.ConvertFromPDF(
+                    $"{strPath}DocConverter.PDF.Input.pdf",
+                    APDocConverter.FromPDFFunction.ToPDFA,
+                    $"{strPath}DocConverter.ConvertPDFToPDFA.Output.pdf");
+            WriteResult(result);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        // Result Handling
+        public static void WriteResult(DCDK.Results.DocConverterResult Result)
+        {
+            Console.WriteLine($"Result Origin: {Result.Origin.Class}.{Result.Origin.Function}");
+            Console.WriteLine($"Result Status: {Result.DocConverterStatus}");
+            if (!string.IsNullOrEmpty(Result.Details))
+            {
+                Console.WriteLine($"Result Details: {Result.Details}");
+            }
+            if (Result.ResultException != null)
+            {
+                Console.WriteLine("Exception caught during conversion.");
+                Console.WriteLine($"Excpetion Details: {Result.ResultException.Message}");
+                Console.WriteLine($"Exception Stack Trace: {Result.ResultException.StackTrace}");
+            }
+        }
+    }
+}
